a48ea4db0ca758dff4a197c4f0e5565814846f92
manlikleo <olaotan104@gmail.com>
2021-08-18 19:36:14 +0300

    creating server side

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require ('express'), 
+    morgan = require ('morgan'),
+    bodyParser = require('body-parser'),
+    cors = require ('cors');
+
+    require('dotenv').config(); 
+
+    const app = express(); 
+
+    app.use(morgan('dev'));
+    app.use(bodyParser.json({limit:'200mb',extended:true}));
+    app.use(bodyParser.urlencoded({limit:'200mb', extended:true}));
+
+    app.use(cors({ origin:"*" })); 
+
+
+    const port = process.env.Port || 8000;
+    
+    app.listen(port, ()=> {
+
+        console.log(`server listening at port ${port}`)
+    })
