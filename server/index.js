6c983119b28fddfdc2576f316311934444f061de
manlikleo <olaotan104@gmail.com>
2021-08-26 20:31:47 +0300

    background script

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
 const express = require ('express'), 
     morgan = require ('morgan'),
     bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
     cors = require ('cors');
 
     require('dotenv').config(); 
 
     const app = express(); 
 
+    mongoose
+            .connect(process.env.DEV_DB, {useUnifiedTopology: true ,useCreateIndex:true,useFindAndModify:false,useNewUrlParser: true})
+            .then(()=> console.log("DB Connected"))
+            .catch(err=> {
+                console.log(err)
+            })
+
     app.use(morgan('dev'));
     app.use(bodyParser.json({limit:'200mb',extended:true}));
     app.use(bodyParser.urlencoded({limit:'200mb', extended:true}));
 
     app.use(cors({ origin:"*" })); 
+
+
+    app.post('/save-product',(req,res)=> {
+        console.log(res.body)
+        res.send('all good')
+    })
 
 
     const port = process.env.PORT || 8000;
