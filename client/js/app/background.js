2df71757001ae1566b5ff4d2dcbe357e7c8ae8ec
manlikleo <olaotan104@gmail.com>
2021-08-26 20:34:09 +0300

    new items added

--- client/js/app/background.js
+++ client/js/app/background.js
@@ -1,1 +1,25 @@
-console.log('we are outside');
+console.log('background script running');
+
+
+
+chrome.runtime.onMessage.addListener(
+
+  function(message,sender,sendResponse){
+
+    // switch statement used to find cases based on the user current state
+    switch (message.type){
+
+      case "login":
+          console.log('login logic ran with FormData ', message.data)
+          return true;
+          break;
+          
+      case "signup": 
+          console.log('signup logic ran with formData',message.data)
+          return true;
+          break;
+
+      default:
+          console.log('no match found')
+    }
+});
--- client/js/app/popup.js
+++ client/js/app/popup.js
@@ -1,4 +1,4 @@
-
+console.log('popup.js initiliazed')
 
 let jumiaScrapper = angular.module("jumiascrapper",['ui.router']);
 
@@ -9,7 +9,49 @@ jumiaScrapper.config(function($stateProvider, $urlRouterProvider){
         url:'/home',
         templateUrl:'../views/home.html'
     })
+    .state('login', {
+        url:'/login',
+        templateUrl:'../views/login.html'
+    })
+    .state('signup', {
+        url:'/signup',
+        templateUrl:'../views/signup.html'
+    })
 
-    $urlRouterProvider.otherwise('home')
+    $urlRouterProvider.otherwise('login')
 
 })
+
+
+jumiaScrapper.controller("popupCtrl", ['$scope', '$state', function($scope,$state){
+    console.log('popupCtrl initialized');
+
+
+
+
+    $scope.login = function(formData){
+        console.log(formData);
+        chrome.runtime.sendMessage({type:"login", data:formData}, 
+            function(response){
+                console.log('response from background is:',response ) 
+            }
+        
+        )
+    }
+
+    $scope.signup  = function(formData){
+        console.log(formData);
+        chrome.runtime.sendMessage({type:"signup",data:formData}, 
+        function(response){
+            console.log('response from background is:',response )
+            // if(response.token){
+            //     $state.go('login');
+            // }
+        
+        }
+        
+        )
+    }
+
+   
+}])
