d5f0d3babd643c555c006ee80dbd4cf2ea3690f1
manlikleo <olaotan104@gmail.com>
2021-12-21 19:53:36 +0000

    landing page fixed

--- webapp/components/Footer.tsx
+++ webapp/components/Footer.tsx
@@ -7,8 +7,6 @@ interface Icons {icon: string,link:string}
 
 export const Footer = () => {
 
-   
-
     const Myicons: Icons [] = [
 
     {link:'https://github.com/manlikleo',icon:"/github.svg",
@@ -25,18 +23,14 @@ export const Footer = () => {
             link:"https://api.whatsapp.com/send?phone=447546979379&text=checking%20out%20your%20extension"
         }
     ]
-
- 
-
-
-
+    
     return(
 
         <div className="bg-primary text-white flex items-center md:justify-between xl:px-24 sm:p-4  lg:px-12 2xl:px-72 lg:py-8 md:flex-row sm:flex-col sm:justify-center md:text-sm sm:text-xs">
 
             <div className="flex justify-between items-center lg:w-[20%] md:w-1/4 sm:w-1/2 sm:mb-4 md:mb-0">
                 { Myicons.map((icons: Icons)=> (
-                    <Link href={icons.link}><a className="text-white "><img src={icons.icon} alt="" /></a></Link>
+                    <Link key={icons.link} href={icons.link}><a className="text-white "><img src={icons.icon} alt="" /></a></Link>
                 ))}
             </div>
             <p className="text-center">© 2021 Robot Dorm a subsidiary of Olaotan Multiventure Limited </p>
--- webapp/components/secondSection.tsx
+++ webapp/components/secondSection.tsx
@@ -2,14 +2,15 @@
 export const SecondSection = () => {
 
     return (
-        <div className="bg-white py-16 px-24 flex items-center justify-center flex-col">
-            <div className="text-center lg:mb-12 2xl:mb-20">
-                <h2 className="lg:text-4xl 2xl:text-5xl xl:font-semibold lg:mb-5 2xl:mb-8">Discover the seller winning in the market</h2>
-                <h4 className="xl:text-xl xl:px-60 lg:px-24 lg:text-base 2xl:px-[35rem] 2xl:text-3xl">With RobotDorm chrome extension you spy on your competitors, have accurate product informations and estimated revenue generated</h4>
+        <div className="bg-white lg:py-16 lg:px-24 sm:p-4 flex items-center justify-center flex-col">
+            <div className="text-center sm:mb-12 2xl:mb-20">
+                <h2 className="lg:text-4xl 2xl:text-5xl sm:text-2xl  font-semibold sm:mb-5 2xl:mb-8">Discover the seller winning in the market</h2>
+                <h4 className="xl:text-xl xl:px-60 lg:px-24 lg:text-base 2xl:px-[35rem] 2xl:text-3xl sm:text-sm">With RobotDorm chrome extension you spy on your competitors, have accurate product informations and estimated revenue generated</h4>
             </div>
-            <div className="xl:w-[55%] lg:w-3/5 2xl:w-2/5">
+            <div className="xl:w-[55%] lg:w-3/5 2xl:w-2/5 md:w-[80%]">
                 <img className="drop-shadow-lg rounded-xl" src="/screens.png" alt="screenshot" />
             </div>
+                <span className="lg:hidden sm:flex bg-primary text-white mt-6 text-base p-4 rounded-xl">We're launching web version soon</span>
         </div>
     )
 
