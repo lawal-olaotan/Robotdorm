e404971a51c7f02dbcf6c323da79e60f52711a17
manlikleo <olaotan104@gmail.com>
2021-12-22 22:52:24 +0000

    deploy

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
-import { Layouts } from "../components/Layouts"
+import { Layouts } from "@components/Layouts"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
