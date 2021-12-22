901ee251ba201a6d4db2050bc3a14d310363eb0e
manlikleo <olaotan104@gmail.com>
2021-12-22 19:39:01 +0000

    build

--- webapp/components/Hero.tsx
+++ webapp/components/Hero.tsx
@@ -1,13 +1,13 @@
-import { Navbar } from "./Navbar"
+
 import { Section }from './Section'
-import { SecondSection } from "./SecondSection"
-import {Footer} from './Footer';
+import { Secondsection } from "./Secondsection"
+
 
  const  Hero = () => {
     return (
         <div>
             <Section sectionSubTitle="Start or scale your jumia business with accurate market insight in minutes." sectionTitle="Free access to best-selling products in a click." imgTitle="/firstpic.png"/>
-            <SecondSection/>
+            <Secondsection/>
             <Section sectionSubTitle="Robotdorm chrome extensions gives you the strength to make important business decisions faster." sectionTitle="Available for free..." imgTitle="/secondpic.png"/>
         </div>
     )
