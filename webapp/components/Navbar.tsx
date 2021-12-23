d63be564aeca4486d680b08fafe37abe2c17c3b5
manlikleo <olaotan104@gmail.com>
2021-12-23 11:20:18 +0000

    deploy

--- webapp/components/Navbar.tsx
+++ webapp/components/Navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
             <div className= { `${active? 'flex' : 'hidden'} z-0 lg:flex xl:w-2/5 lg:w-2/4 lg:justify-between lg:flex-row items-center lg:p-0 lg:h-auto lg:bg-transparent lg:relative lg:text-base text-white sm:absolute sm:flex-col sm:w-full sm:top-0 sm:left-0 sm:pt-20 sm:bg-primary sm:h-screen sm:self-start sm:flex-start sm:text-2xl sm:z-10`}   >
 
                 <button className=" lg:hidden absolute top-6 right-6" onClick={handleButton}>
-                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                 </button>
 
                 <Navitems routeName="Sell on jumia" routeLink="https://www.youtube.com/results?search_query=selling+on+jumia" />
@@ -28,7 +28,7 @@ export const Navbar = () => {
 
             </div>
             <button className="lg:hidden inline-flex outline-none" onClick={handleButton}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-menu text-white"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" strokeLinejoin="round" className="feather feather-menu text-white"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
             </button>
 
         </nav>
