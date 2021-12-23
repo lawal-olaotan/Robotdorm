add3549f4fbc7e62072568c37afa5bbf95cfb8c7
manlikleo <olaotan104@gmail.com>
2021-12-23 11:52:48 +0000

    espace functionalities

--- webapp/components/Second.tsx
+++ webapp/components/Second.tsx
@@ -2,13 +2,13 @@ export const Second = () => {
     return (
         <div className="bg-white lg:py-16 lg:px-24 sm:p-4 flex items-center justify-center flex-col">
             <div className="text-center sm:mb-12 2xl:mb-20">
-                <h2 className="lg:text-4xl 2xl:text-5xl sm:text-2xl  font-semibold sm:mb-5 2xl:mb-8">Discover the seller winning in the market</h2>
+                <h2 className="lg:text-4xl 2xl:text-5xl sm:text-2xl font-semibold sm:mb-5 2xl:mb-8">Discover the seller winning in the market</h2>
                 <h4 className="xl:text-xl xl:px-60 lg:px-24 lg:text-base 2xl:px-[35rem] 2xl:text-3xl sm:text-sm">With RobotDorm chrome extension you spy on your competitors, have accurate product informations and estimated revenue generated</h4>
             </div>
             <div className="xl:w-[55%] lg:w-3/5 2xl:w-2/5 md:w-[80%]">
                 <img className="drop-shadow-lg rounded-xl" src="/screens.png" alt="screenshot" />
             </div>
-                <span className="lg:hidden sm:flex bg-primary text-white mt-6 text-base p-4 rounded-xl">We're launching web version soon</span>
+                <span className="lg:hidden sm:flex bg-primary text-white mt-6 text-base p-4 rounded-xl">We&apos;re launching web version soon</span>
         </div>
     )
 }
