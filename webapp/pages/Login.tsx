79c29c200582e7e2af9057dee3ae957761f27b96
manlikleo <olaotan104@gmail.com>
2022-01-22 19:03:27 +0000

    session features implementation

--- webapp/pages/Login.tsx
+++ webapp/pages/Login.tsx
@@ -1,52 +1,39 @@
 import  React, {useEffect,useState,useContext} from 'react'
 import type { NextPage } from 'next';
 import { useRouter} from 'next/router'
-import { getSession } from 'next-auth/react'; 
+import { useSession } from 'next-auth/react'; 
 import AuthForm from '@components/AuthForm';
-import {MyContext} from '../lib/UserContext'
-
-
 
 
 const Login: NextPage = () => {
 
-    const {SetMyId} = useContext(MyContext); 
-
+    const {data:session,status} = useSession()
     const [loading, SetLoading] = useState(true); 
-    const router = useRouter(); 
-
-    useEffect(()=> {
-        getSession()
-        .then(async (session) => {
-            if(session){
-                const data = await getUserInfo()
-                if(data){
-                    router.replace('/Dashboard'); 
-                }
-            }else{
-                SetLoading(false)
-            }
-        })
-    },[router])
-
-    let myIdData : any;
-
-    const getUserInfo = async() => {
-
-        const extensionId: string = 'llneclmbomnmhcgbaacmjdloencbfahj'; 
-
-        await fetch('/api/getInfo', {method: 'GET',headers:{'Content-Type': 'application/json;'} })
-        .then((response)=> response.json())
-        .then((mydata)=> {
-            let {id, ...newData} = mydata.data; 
-            myIdData = newData; 
-            SetMyId(newData);
-        })
-
-        chrome.runtime.sendMessage(extensionId, {type:'browser',data:myIdData.id})
-         
-        return true; 
+    const router = useRouter();
+    
+    const extensionId: string = 'llneclmbomnmhcgbaacmjdloencbfahj';
+    
+    if(status === 'authenticated'){
+       chrome.runtime.sendMessage(extensionId, {type:'browser',data:session.user.id})
+        router.replace('/Dashboard');
+    }else{
+        SetLoading(false)
     }
+    // let myIdData : any;
+    // const getUserInfo = async() => {
+
+    //     await fetch('/api/getInfo', {method: 'GET',headers:{'Content-Type': 'application/json;'} })
+    //     .then((response)=> response.json())
+    //     .then((mydata)=> {
+    //         let {id, ...newData} = mydata.data; 
+    //         myIdData = newData; 
+    //         SetMyId(newData);
+    //     })
+
+    //     chrome.runtime.sendMessage(extensionId, {type:'browser',data:myIdData.id})
+         
+    //     return true; 
+    // }
 
     if(loading){
         return <p>Loading Page</p>
