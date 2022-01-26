c5e06b68748decb7ae16cdb242344c7dc8e80704
manlikleo <olaotan104@gmail.com>
2022-01-26 12:03:25 +0000

    login feature

--- webapp/components/AuthForm.tsx
+++ webapp/components/AuthForm.tsx
@@ -31,7 +31,7 @@ const AuthForm = ()=> {
     
 
         const result = await signIn('email', {
-            redirect:false,
+            callbackUrl:'/Dashboard',
             email: InputEmail
         });
         
