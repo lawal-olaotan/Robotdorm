1fc1e630c1891f5bb38652610d2e869f87cf7d97
manlikleo <olaotan104@gmail.com>
2022-02-20 14:54:30 +0000

    submit validation

--- webapp/pages/Login.tsx
+++ webapp/pages/Login.tsx
@@ -23,7 +23,7 @@ const Login: NextPage = () => {
                     let extensionKey = localStorage.getItem('userkey'); 
                     chrome.runtime.sendMessage(EXE_ID, {type:'browser',data:extensionKey}) 
                 }
-                router.push('/Dashboard');
+                router.push('/Login');
             }else{
                 SetLoading(false)
             }
