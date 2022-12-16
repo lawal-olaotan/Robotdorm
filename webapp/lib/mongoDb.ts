1c93136e82b6d6d5c1cfd79ff42eb00cf0eb3bdd
Olaotan Lawal <olaotan104@gmail.com>
2022-12-16 22:10:08 +0000

    fixing next-auth issue still

--- webapp/lib/mongoDb.ts
+++ webapp/lib/mongoDb.ts
@@ -6,24 +6,23 @@ const PORT = process.env.NEXTAUTH_URL;
 
  const options:any = {}
 
- let client
+
  let ClientPromise:Promise<MongoClient>
 
  if(!process.env.MONGO_URI){
      throw new Error('Please add your mongo URI')
  }
 
- if(!PORT){
-     client = new MongoClient(URI,options)
+     let client = new MongoClient(URI,options)
      ClientPromise = client.connect(); 
- }else{
+
 
     if(!global._MongoClientPromise){
         client = new MongoClient(URI,options); 
         global._mongoClientPromise = client.connect(); 
     }
     ClientPromise = global._mongoClientPromise
- }
+ 
 
 
 export default ClientPromise
