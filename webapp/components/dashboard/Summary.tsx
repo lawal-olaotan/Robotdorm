4610f6871dafea2f154a162979a605319de53ef6
Olaotan Lawal <olaotan104@gmail.com>
2022-12-16 21:09:29 +0000

    created search summary component

---
+++ webapp/components/dashboard/Summary.tsx
@@ -0,0 +1,42 @@
+import { NextPage } from 'next';
+
+
+interface Props{
+    _id: string,
+    keyWord:string,
+    EstTotalRevenue: string,
+    EstAverageRevenue: string,
+    EstTotalUnitsSold: string,
+    AveragePrice:string
+}
+
+export const Summary: NextPage<Props> = (Props) => {
+
+    const {_id, keyWord, EstTotalRevenue,   EstAverageRevenue, EstTotalUnitsSold, AveragePrice} = Props
+
+    return (
+        <>
+         <div key={_id} className="flex flex-col p-8 bg-white mr-8 rounded-lg mb-8 shadow-6xl w-[32%]">
+                        <p className="mb-6 font-semibold"> <span>Keyword:</span> <span className="text-secondary">{keyWord}</span> </p>
+                        <div className="flex">
+                            <p className="flex flex-col text-center mr-6">
+                                <span  className="text-xs mb-2 text-gray-600">Est. Total Revenue</span>
+                                <span className="text-secondary font-semibold">{EstTotalRevenue}</span>
+                            </p>
+                            <p className="flex flex-col text-center mr-6">
+                                <span className="text-xs mb-2 text-gray-600">Est. Avg Revenue</span>
+                                <span className="text-secondary font-semibold">{EstAverageRevenue}</span>
+                            </p>
+                            <p className="flex flex-col text-center mr-6">
+                                <span className="text-xs mb-2 text-gray-600">Est. Total Units Sold</span>
+                                <span className="text-secondary font-semibold">{EstTotalUnitsSold}</span>
+                            </p>
+                            <p className="flex flex-col text-center mr-6">
+                                <span className="text-xs mb-2 text-gray-600">Average Price</span>
+                                <span className="text-secondary font-semibold">{AveragePrice}</span>
+                            </p>
+                        </div>
+        </div>
+        </>
+    )
+ }
