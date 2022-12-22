fd4d2d14f691ea812fd351e9df4ba9b6e1e6e762
manlikleo <olaotan104@gmail.com>
2022-12-22 22:08:47 +0000

    fixing deployment key

--- webapp/components/dashboard/Summary.tsx
+++ webapp/components/dashboard/Summary.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 
 interface Props{
-    keys: number,
+    key: number,
     keyWord:string,
     EstTotalRevenue: string,
     EstAverageRevenue: string,
@@ -12,11 +12,11 @@ interface Props{
 
 export const Summary: NextPage<Props> = (Props) => {
 
-    const {keys, keyWord, EstTotalRevenue,EstAverageRevenue, EstTotalUnitsSold, AveragePrice} = Props
+    const {key, keyWord, EstTotalRevenue,EstAverageRevenue, EstTotalUnitsSold, AveragePrice} = Props
 
     return (
         <>
-         <div key={keys} className="flex flex-col p-8 bg-white mr-8 rounded-lg mb-8 shadow-6xl w-1/5">
+         <div key={key} className="flex flex-col p-8 bg-white mr-8 rounded-lg mb-8 shadow-6xl w-1/5">
                         <p className="mb-6 font-semibold"> <span>Keyword:</span> <span className="text-secondary">{keyWord}</span> </p>
                         <div className="flex flex-col">
                             <div className="flex mb-4">
