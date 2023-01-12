47a881630e846933a0f107b45a2b6a77a4b3e853
manlikleo <olaotan104@gmail.com>
2023-01-12 00:15:06 +0000

    Added extension CTA

--- webapp/components/Hero.tsx
+++ webapp/components/Hero.tsx
@@ -4,7 +4,7 @@ import { Second } from './Second'
 export const Hero = () => {
     return (
         <div>
-            <Section sectionSubTitle="With RobotDorm extension you can Start or scale your jumia business with accurate market insight in minutes." sectionTitle="Have access to best-selling products in a click." imgTitle="/firstpic.png"/>
+            <Section sectionSubTitle="With RobotDorm jumia keyword extension you can start or scale your jumia business with accurate market insight in minutes." sectionTitle="Access best-selling products data in a click." imgTitle="/firstpic.png"/>
             <Second/>
             <Section sectionSubTitle="Robotdorm chrome extension gives you the right data to make important business decisions faster." sectionTitle="Available for free..." imgTitle="/secondpic.png"/>
         </div>
