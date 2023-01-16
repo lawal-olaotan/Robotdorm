5d172b9f723f2e7ee3a7b75b861ef74ef1975f46
manlikleo <olaotan104@gmail.com>
2023-01-16 07:30:20 +0000

    Refined install page

--- webapp/pages/install.tsx
+++ webapp/pages/install.tsx
@@ -17,14 +17,14 @@ export default function Install(){
             <div className="xl:w-1/2 lg:w-10/12 m-auto"> 
            
             <div className="w-full my-4 text-center">
-                <h1 className="font-bold text-2xl">Thanks you for installing our extension</h1>
+                <h1 className="font-bold text-2xl mb-2">Thanks you for installing our extension</h1>
                 <p className="mb-4">To take full advantage of our Jumia keyword tool. It is highly recommended you check out the two videos below. </p>
                 <p className="text-red-900">Note: to expand the videos below, kindly click the youtube link instead of expanding the video here.</p>
             </div>
             <div>
-            <YoutubeWidgets title="How to set up your jumia keyword tool" description="This video takes you through how to properly setup your how you need to setup your jumia keyword tool"  videoId="_CxA9hneGkg"/>
+            <YoutubeWidgets title="How to set up your jumia keyword tool" description="This video takes you through how to properly setup your your jumia keyword tool."  videoId="_CxA9hneGkg"/>
 
-            <YoutubeWidgets title="Finding validated products on jumia" description="This video takes you through how to find already validated products that you could add to your store and start making profit" videoId="8FEhz2-Hq8A"/>   
+            <YoutubeWidgets title="Finding validated products on jumia" description="This video takes you through how to find already validated products that you could add to your store and start making profit." videoId="8FEhz2-Hq8A"/>   
             </div>
 
             </div>
