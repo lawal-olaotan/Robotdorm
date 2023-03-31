8df2cb612025dd52a18f399a372904c26b33e598
Olaotan Lawal <olaotan104@gmail.com>
2023-03-31 08:43:42 +0100

    fixes extension and webapp message bug

--- webapp/pages/Login.tsx
+++ webapp/pages/Login.tsx
@@ -13,9 +13,10 @@ const Login: NextPage = () => {
     useEffect(()=> {
         getSession()
         .then((session) => {
+
+        if(session){
             // TODO: this is a temporary fix, looking for a roboust approach to sending message to the Extension
             chrome.runtime.sendMessage('iebnenlmoeolohhmbjilijlgpjbjljhm', {type:'browser',data:session.user.id})
-            if(status == 'authenticated'){
             router.push('/Dashboard');
         }else{
             SetLoading(false)
