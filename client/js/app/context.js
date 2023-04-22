d71af8b05acf5d8828226397c19064f268a26fa0
manlikleo <olaotan104@gmail.com>
2023-04-22 09:28:17 +0100

    Removing merge tags

--- client/js/app/context.js
+++ client/js/app/context.js
@@ -7,7 +7,6 @@ localStorage.removeItem('list');
 
 // checking webpage and sending boolean response to popup js for view manipulation. 
 const jumiaDomainExtensions = [
-<<<<<<< HEAD
     'https://www.jumia.com.ng/',
     'https://www.jumia.dz/',
     'https://www.jumia.ma/',
@@ -17,31 +16,16 @@ const jumiaDomainExtensions = [
     'https://www.jumia.co.ke',
     'https://www.jumia.ma/',
     'https://www.jumia.sn/',
-=======
-    'https://www.jumia.com.ng',
-    'https://www.jumia.ma',
-    'https://www.jumia.com.eg',
-    'https://www.jumia.com.gh/',
-    'https://www.jumia.ci',
-    'https://www.jumia.co.ke',
-    'https://www.jumia.ma',
-    'https://www.jumia.sn',
->>>>>>> a79b2fc31ba17fffc7b6fdcb36f77579a418f42f
     'https://www.zando.co.za/',
     'https://www.jumia.com.tn/',
     'https://www.jumia.ug/'
 ]
 
-<<<<<<< HEAD
 // checks if domain extension is contains a subString of the url
 const checkUrlExtension = (domainUrl) => url.includes(domainUrl); 
 
 if(!jumiaDomainExtensions.some(checkUrlExtension)){
     console.log(url);
-=======
-// TODO: I'm not sure this is right approach you can kindly review and let me know your thoughts
-if(!jumiaDomainExtensions.includes(url)){
->>>>>>> a79b2fc31ba17fffc7b6fdcb36f77579a418f42f
     sendToPopup(false, 'mypage')
 }else{
     sendToPopup(true, 'mypage'); 
@@ -50,25 +34,12 @@ if(!jumiaDomainExtensions.includes(url)){
         const urlArray = []
         let searchLink = url;
         urlArray.push(searchLink);
-<<<<<<< HEAD
 for(let i =2; i <= 25; i++){   
-=======
-        for(let i =2; i <= 40; i++){
->>>>>>> a79b2fc31ba17fffc7b6fdcb36f77579a418f42f
             let searchlinks = `${url}&page=${[i]}#catalog-listing`;
             urlArray.push(searchlinks);
         }
         let queryData = document.querySelector('.brcbs.col16.-pvs').lastChild.innerHTML;
-<<<<<<< HEAD
         const searchData = {searchLink : urlArray , keyWord : queryData};
-=======
-
-        // Price symbol was investigated across all website
-        let PriceSymbol = document.querySelector(".-m.-upp.-fs14.-pvs").innerHTML; 
-        let currency = PriceSymbol.split(' ')[1].replaceAll(/\().+?\)/, "");
-
-        const searchData = {searchLink : urlArray , keyWord : queryData, currency:currency};
->>>>>>> a79b2fc31ba17fffc7b6fdcb36f77579a418f42f
         
         const PrevKey = localStorage.getItem('Keyword');
         
