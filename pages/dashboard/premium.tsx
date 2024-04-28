7d15288014fd68dec728417c9f8b3fd37ee5f279
Olaotan Lawal <olaotan104@gmail.com>
2024-04-29 00:37:08 +0100

    Changes to premium page

--- pages/dashboard/premium.tsx
+++ pages/dashboard/premium.tsx
@@ -2,28 +2,25 @@ import { DashLayout } from '@components/dashboard/DashLayout';
 import { DashHead } from '@components/dashboard/DashHead';
 import { DashTitle } from '@components/dashboard/DashTitle';
 import React,{ useEffect} from 'react';
-import { extensionId } from "../../extension"
 import Lottie from "lottie-react";
-import AnimationData from "../../lottie/premium.json";
+import AnimationData from "../../lottie/success.json";
+import Link from 'next/link';
 
 export default function Account(){
-    useEffect(()=> {
-        setTimeout(() => {
-            chrome.runtime.sendMessage(extensionId,{type:"premium", data:''})
-        }, 3000);
-        
-    },[])
+
     return (
         <>
-        <DashHead PageName="Payment confirmed"/> 
-        <div>
-        <DashTitle DashTitle="Payment successful"/>
-        <div className="mt-8 flex flex-col items-center">
-            <div className="h-[400px] w-[400px] flex mb-4">
-                <Lottie animationData={AnimationData} />
+        <DashHead PageName="Success"/> 
+        <div className='flex items-center justify-center'>
+            <div className="mt-8 text-center bg-[#FAFAFA] w-fit p-8 rounded-md shadow-md">
+                <h2 className="font-black text-2xl">Thank you!</h2>
+                <div className="w-[400px] flex items-center justify-center mb-4 m-auto">
+                    <Lottie animationData={AnimationData} />
+                </div>
+                <p className='w-3/5 m-auto text-sm my-8 font-medium'>You can now unleash the power of robotdorm to grow your ecommerce business.</p>
+
+                <Link href="/dashboard"><a className='px-8 py-3 bg-[#307AD1] text-white rounded-md'>Get started now</a></Link>
             </div>
-            <div className='w-1/2 font-bold'>Redirecting you back to your keyword search to enjoy unlimited search</div>
-        </div>
         </div>
         </>
     )    
