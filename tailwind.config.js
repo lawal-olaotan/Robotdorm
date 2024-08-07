3764e276825fcf0adaf5413c3d353f772702f0b2
Olaotan Lawal <olaotan104@gmail.com>
2024-08-07 12:47:44 +0100

    Commit adds other config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         md: "768px",
         lg: "1024px",
         xl: "1440px",
+        "2xl":'1920px'
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
